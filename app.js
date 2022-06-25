f9e586296cb7aada0f0a99bd818a0f19acc18b47
goforpordev <goforprodev@gmail.com>
2022-06-25 19:50:16 +0100

    inital express setup

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-const http = require("http");
-const prom = "prom"
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const express = require('express')
 
-  if (url === "/") {
-    res.write("<html");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Add User</button></form></body>'
-    );
-    res.write("</html>");
-    res.end();
-  }
-
-  if (url === "/create-user" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-  }
-});
-
-server.listen(3000);
+const http = require('http')
